f6bf0e9d33a2c92e580f4c1728815aed81b8fc41
kirdemorgan <drankov.alex@yandex.ru>
2024-09-27 20:41:08 +0500

    lab task 2

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -7,6 +7,7 @@ class Program
         Program p = new Program();
 
         Console.WriteLine("Lab 1 variant 2");
+        Console.WriteLine("Tasks 2 4 6 8 10\n");
         Console.WriteLine("Enter a task number: ");
 
         int choice = Convert.ToInt32(Console.ReadLine());
@@ -14,11 +15,9 @@ class Program
         switch (choice)
         {
             case 1:
-                Console.WriteLine("Tasks 2 4 6 8 10");
                 Console.WriteLine("\nEnter a number to test task 2: ");
                 int x = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Sum of 2 last digits: "+ p.sumLastNums(x));
-
 
                 Console.WriteLine("\nEnter a number to test task 4: ");
                 int y = Convert.ToInt32(Console.ReadLine());
@@ -39,6 +38,30 @@ class Program
                 Console.WriteLine(p.lastNumSum(c, d));
                 break;
             case 2:
+                Console.WriteLine("\nEnter 2 numbers to test task 2: ");
+                x = Convert.ToInt32(Console.ReadLine());
+                y = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(p.safeDiv(x, y));
+
+                Console.WriteLine("\nEnter 2 numbers to test task 4: ");
+                x = Convert.ToInt32(Console.ReadLine());
+                y = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(p.makeDecision(x, y));
+
+                Console.WriteLine("\nEnter 3 numbers to test task 6: ");
+                x = Convert.ToInt32(Console.ReadLine());
+                y = Convert.ToInt32(Console.ReadLine());
+                c = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(p.sum3(x, y, c));
+
+                Console.WriteLine("\nEnter a number to test task 8: ");
+                x = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(p.age(x));
+
+                Console.WriteLine("\nEnter a day of the week to test task 10: ");
+                string day = Console.ReadLine();
+                p.printDays(day);
+
                 break;
             case 3:
                 break;
@@ -52,6 +75,7 @@ class Program
 
 
     /*========================================================================
+     Задание 1. Методы
      ========================================================================*/
     // 1.2
     public int sumLastNums(int x)
@@ -100,7 +124,69 @@ class Program
     }
 
     /*========================================================================
+     Задание 2. Условия
  ========================================================================*/
+    // 2.2
+    public double safeDiv(int x, int y)
+    {
+        if (y == 0)
+        {
+            return 0;
+        }
+        return (double)x / y;
+    }
 
+    // 2.4
+    public String makeDecision(int x, int y)
+    {
+        if (x > y) return $"{x} > {y}";
+        if (x < y) return $"{x} < {y}";
+        return $"{x} == {y}";
+    }
 
+    // 2.6
+    public bool sum3(int x, int y, int z)
+    {
+        return (x + y == z) || (x + z == y) || (y + z == x);
+    }
+
+    // 2.8
+    public String age(int x)
+    {
+        if ((x % 10 == 1) && x != 11) return $"{x} год";
+        if (x % 10 >= 2 && x % 10 <= 4 && x != 12 && x != 13 && x != 14) return $"{x} года";
+        return $"{x} лет";
+    }
+
+    // 2.10
+    public void printDays(string x)
+    {
+        switch (x.ToLower())
+        {
+            case "понедельник":
+                Console.WriteLine("понедельник");
+                goto case "вторник";
+            case "вторник":
+                Console.WriteLine("вторник");
+                goto case "среда";
+            case "среда":
+                Console.WriteLine("среда");
+                goto case "четверг";
+            case "четверг":
+                Console.WriteLine("четверг");
+                goto case "пятница";
+            case "пятница":
+                Console.WriteLine("пятница");
+                goto case "суббота";
+            case "суббота":
+                Console.WriteLine("суббота");
+                goto case "воскресенье";
+            case "воскресенье":
+                Console.WriteLine("воскресенье");
+                break;
+            default:
+                Console.WriteLine("это не день недели");
+                break;
+        }
+    }
 }
--- lab1/obj/Debug/net8.0/lab1.AssemblyInfo.cs
+++ lab1/obj/Debug/net8.0/lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f76f3f3901527f850470d0f7385a27204b4613ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
