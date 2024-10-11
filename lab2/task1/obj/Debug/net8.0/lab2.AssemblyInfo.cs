309823a7dff0a94f2439361ddd95c28f302dc8d9
kirdemorgan <drankov.alex@yandex.ru>
2024-10-11 17:50:10 +0500

    lab2 task1

--- lab2/task1/obj/Debug/net8.0/lab2.AssemblyInfo.cs
+++ lab2/task1/obj/Debug/net8.0/lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d708c748bbacc50679fa207601803bfb64cfcad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a2938294aa3ad52b53a761f347543b6202a2143")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
